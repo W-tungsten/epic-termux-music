7fa7e06a332b629d687b9f7ac2e94ae530b2e2cb
W-tungsten <129035838+W-tungsten@users.noreply.github.com>
2023-06-11 20:52:03 +0100

    Create epictermuxmusic1.sh

---
+++ epictermuxmusic1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+install(){
+pkg i -y figlet toilet
+# You need Termux API as well
+}
+
+music(){
+ls ~ |
+toilet |
+termux-tts-speak \
+-s MUSIC \ # sets stream to music, gets around do not disturb
+-r 2 \ # double speed
+-p 1.8 \ # 1.8x pitch
+-l jp # japanese
+}
